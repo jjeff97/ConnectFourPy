fc1892c2ff0b5b786a2cfb7d9e7c93e3f5dfebde
James Jefferson <jrj72@Jamess-Mac-mini.local>
2021-06-21 21:51:03 -0500

    player input feature build cont

--- connect4.py
+++ connect4.py
@@ -11,8 +11,13 @@ turn = 0
 while not game_over:
     # ask for player 1 input
     if turn == 0:
-        selection = input("Player 1 Make your selection (0-6):")
+        selection = int(input("Player 1 Make your selection (0-6):"))
         
-        print
+
     
     #ask for player 2 input
+    else:
+        selection = int(input("Player 2 Make your selection (0-6):"))
+        
+    turn += 1
+    turn = turn % 2
