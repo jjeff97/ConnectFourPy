0c2ffb922d73c54779ad877774903c107929f3f3
James Jefferson <jrj72@Jamess-Mac-mini.local>
2021-06-21 23:04:40 -0500

    game play set up

--- connect4.py
+++ connect4.py
@@ -2,22 +2,30 @@ import numpy as np
 def create_board():
     board = np.zeros((6,7))
     return board
+def drop_piece():
+    pass
+
+def is_valid_location(board, col):
+    return board[5][col] == 0
+
+def get_next_open_row():
+    pass
 
 board = create_board()
-
+print(board)
 game_over = False
 turn = 0
 
 while not game_over:
     # ask for player 1 input
     if turn == 0:
-        selection = int(input("Player 1 Make your selection (0-6):"))
+        col = int(input("Player 1 Make your selection (0-6):"))
         
 
     
     #ask for player 2 input
     else:
-        selection = int(input("Player 2 Make your selection (0-6):"))
+        col = int(input("Player 2 Make your selection (0-6):"))
         
     turn += 1
     turn = turn % 2
